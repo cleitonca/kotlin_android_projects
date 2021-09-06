bc526252491f94c74cb384c2782f08e0366cdb6f
Cleiton Araújo <cltn.info@gmail.com>
2021-09-06 19:13:26 -0300

    Projeto completo

--- Randomicos/app/src/main/java/br/com/cleitonaraujo/randomicos/MainActivity.kt
+++ Randomicos/app/src/main/java/br/com/cleitonaraujo/randomicos/MainActivity.kt
@@ -2,10 +2,37 @@ package br.com.cleitonaraujo.randomicos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Style "Theme.AppCompat.Light.NoActionBar" esconde também a status bar e deixa transparente
+        // O método abaixo certifica que a Action bar seja escondida
+        //if (supportActionBar != null) {
+        //    supportActionBar!!.hide()
+
+        setListeners()
+        }
+    /**
+     * Qualquer evento de click disparado aciona o método
+     */
+    override fun onClick(v: View?) {
+         //Validação de qual elemento foi clicado
+         if(v?.id == R.id.buttomRandom || v?.id == R.id.textNumber){
+             textNumber.text = random().toString()
+         }
+     }
+     private fun random(): Int {
+        return Random().nextInt(50)
+     }
+
+    private fun setListeners() {
+        buttomRandom.setOnClickListener(this)
+        textNumber.setOnClickListener(this)
     }
 }
